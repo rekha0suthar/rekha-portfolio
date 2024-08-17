5605bc83a1576dceb520026e527a42f475c61ce7
REKHA SUTHAR <rekha0suthar@gmail.com>
2024-08-17 14:12:40 +0530

    link updates to open in new page

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,11 @@ const Home = () => {
           <strong className="title">Software Developer</strong>
         </p>
         <div className="socila-icon">
-          <SocialIcon url="https://www.linkedin.com/in/rekha0suthar/" />{' '}
-          <SocialIcon url="https://github.com/rekha0suthar/" />
+           <SocialIcon
+            url="https://www.linkedin.com/in/rekha0suthar/"
+            target="_blank"
+          />{' '}
+          <SocialIcon url="https://github.com/rekha0suthar/" target="_blank" />
         </div>
       </div>
     </div>
