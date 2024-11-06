90e86937f1fa7309d4c436a48bf40d5423edad8d
REKHA SUTHAR <rekha0suthar@gmail.com>
2024-11-06 19:00:58 +0530

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,14 +4,14 @@ import React from 'react';
 const experiences = [
   {
     companyName: 'Clutterbot',
-    title: 'Data Platform Developer',
+    title: 'Full Stack Developer',
     workingPeriod: 'FEB 2023 - NOV 2023',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI9134w2feR4zJJ4_23uJYxNF7m3DWWYG1JA&s',
     summary: [
-      'Hiring and managing a productive 15-member team for image segmentation and object detection increased productivity by 30%.', 
-      'Utilizing and customizing the CVAT tool as per requirements using MERN stack annotations increased accuracy of tools by 10% and saved 15% of time.', 
-      'Implemented features to improve work speed and quality of the work to enhance model performance.',
-      'Conducting daily meetings to ensure smooth communication, progress updates, and issue resolution.',
+      'Led a 15-member team for image segmentation and object detection, boosting productivity by 30%.', 
+      'Contributed to open-source data annotation platform called CVAT.', 
+      'Developed features such as allows managers to directly import data from AWS S3, one-click annotation, easy drawing, and label customization, improving work speed and quality by 5%.',
+      'Facilitated daily meetings to streamline communication, track progress, and resolve issues efficiently.',
     ],
   },
   {
@@ -21,8 +21,8 @@ const experiences = [
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/oikb5uteskk4bpflrgdi',
     summary: [
       'Developed and implemented smooth, responsive and interactive features using programming language JavaScript and Framework React and Redux for State Management.',
-      "By utilizing a third-party library to add classes, I saved 5% of the development time compared to building it from scratch and increased the application's performance by 3%.",
-      'Collaborative testing(Integration testing and System testing - Black box technique) of features of product with team and bugs and error fixing increased accuracy and performance of product by 15%.',
+      'Integrated a third-party library to streamline development, saving 5% of time and boosting application performance by 3%',
+      'Collaborated on integration and system testing using black box techniques, improving product accuracy and performance by 15%',
     ],
   },
   {
@@ -31,8 +31,8 @@ const experiences = [
     workingPeriod: 'JAN 2022 - MAR 2022',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1494841318/ljxjdluf0nhvinmdcwoq.png',
     summary: [
-      'I crafted engaging and responsive user interfaces, ensuring seamless functionality across devices.',
-      'I implemented personalized information sections and dynamic testimonials using sliding card components and modals, which significantly enhanced the interactivity and visual appeal of the website',
+      'Designed engaging, responsive user interfaces to ensure seamless functionality across devices.',
+      'Enhanced website interactivity and visual appeal by implementing personalized information sections and dynamic testimonials using sliding cards and modals.,
     ],
   },
 ];
